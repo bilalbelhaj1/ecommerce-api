4bac81dc9c0d60e6dae7dd9c3bb754366eef998d
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-04 17:56:00 +0000

    data modeling creating Category Entity

---
+++ src/main/java/org/example/ecommerceapi/entity/Category.java
@@ -0,0 +1,42 @@
+package org.example.ecommerceapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author $(bilal belhaj)
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false ,columnDefinition = "TEXT", length = 100)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate()
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
