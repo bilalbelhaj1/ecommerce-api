cf9cecac884ebce37a8c202eaa534812ffcc77c5
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-04 18:19:42 +0000

    create Customer Entity

---
+++ src/main/java/org/example/ecommerceapi/entity/Customer.java
@@ -0,0 +1,42 @@
+package org.example.ecommerceapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author $(bilal belhaj)
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
+    private String lastName;
+
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT", unique = true)
+    private String email;
+
+    @Column(name = "phone_number", unique = true ,columnDefinition = "TEXT")
+    private String phoneNumber;
+
+    @Column(name = "password_hash", nullable = false, columnDefinition = "TEXT")
+    private String passwordHash;
+
+    @Column(name = "address", columnDefinition = "TEXT")
+    private String address;
+}
