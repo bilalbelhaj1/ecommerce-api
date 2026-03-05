682250aa600f9f80988aac28a253218f55cb36f5
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-05 12:24:00 +0000

    test the Entities

--- src/main/java/org/example/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/org/example/ecommerceapi/EcommerceApiApplication.java
@@ -2,12 +2,26 @@ package org.example.ecommerceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("api")
 public class EcommerceApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApiApplication.class, args);
+    }
+
+
+    @GetMapping
+    public String home() {
+        return "Welcome";
     }
 
 }
