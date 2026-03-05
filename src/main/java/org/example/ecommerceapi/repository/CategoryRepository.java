ea5676c46aac466d0b65004b2624a639c3bf24b9
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-05 11:18:06 +0000

    improve repos design

--- src/main/java/org/example/ecommerceapi/repository/CategoryRepository.java
+++ src/main/java/org/example/ecommerceapi/repository/CategoryRepository.java
@@ -2,6 +2,7 @@ package org.example.ecommerceapi.repository;
 
 import org.example.ecommerceapi.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
