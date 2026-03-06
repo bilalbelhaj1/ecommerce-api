ce97981faa253ba45ab489dc58655b6691ff182d
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-06 13:04:38 +0000

    implement Product mapper in product service

--- src/main/java/org/example/ecommerceapi/service/ProductService.java
+++ src/main/java/org/example/ecommerceapi/service/ProductService.java
@@ -7,6 +7,7 @@ import org.example.ecommerceapi.dto.product.ProductResponseDTO;
 import org.example.ecommerceapi.dto.product.UpdateProductDTO;
 import org.example.ecommerceapi.entity.Category;
 import org.example.ecommerceapi.entity.Product;
+import org.example.ecommerceapi.mapper.ProductMapper;
 import org.example.ecommerceapi.repository.CategoryRepository;
 import org.example.ecommerceapi.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,17 +37,7 @@ public class ProductService {
     public List<ProductResponseDTO> getAllProducts() {
         return productRepository.findAll()
                 .stream()
-                .map(p-> new ProductResponseDTO(
-                        p.getId(),
-                        p.getName(),
-                        p.getDescription(),
-                        p.getPrice(),
-                        p.getStock(),
-                        p.getImageUrl(),
-                        new CategorySummaryDTO(
-                                p.getCategory().getName(),
-                                p.getCategory().getId()
-                        )))
+                .map(ProductMapper::toDTO)
                 .toList();
     }
 
@@ -55,17 +46,7 @@ public class ProductService {
         Product product = productRepository.findById(id).orElseThrow(
                 () -> new IllegalStateException("Product Not found")
         );
-        return new ProductResponseDTO(
-                product.getId(),
-                product.getName(),
-                product.getDescription(),
-                product.getPrice(),
-                product.getStock(),
-                product.getImageUrl(),
-                new CategorySummaryDTO(
-                        product.getCategory().getName(),
-                        product.getCategory().getId()
-                ));
+        return ProductMapper.toDTO(product);
     }
     // add product
     public ProductResponseDTO addProduct(CreateProductDTO createProductDTO) {
@@ -75,27 +56,8 @@ public class ProductService {
         Category cat = categoryRepository.findById(createProductDTO.categoryId()).orElseThrow(
                 () -> new IllegalStateException("Category not found")
         );
-        Product product = Product.builder()
-                .name(createProductDTO.name())
-                .description(createProductDTO.description())
-                .price(createProductDTO.price())
-                .stock(createProductDTO.stock())
-                .imageUrl(createProductDTO.imageUrl())
-                .category(cat)
-                .build();
-        Product saved = productRepository.save(product);
-        return new ProductResponseDTO(
-                saved.getId(),
-                saved.getName(),
-                saved.getDescription(),
-                saved.getPrice(),
-                saved.getStock(),
-                saved.getImageUrl(),
-                new CategorySummaryDTO(
-                        saved.getCategory().getName(),
-                        saved.getCategory().getId()
-                )
-        );
+        Product saved = productRepository.save(ProductMapper.toEntity(createProductDTO, cat));
+        return ProductMapper.toDTO(saved);
     }
     // update product
     public ProductResponseDTO updateProduct(Long id, UpdateProductDTO updateProductDTO) {
@@ -146,18 +108,7 @@ public class ProductService {
             product.setCategory(category);
         }
         Product saved = productRepository.save(product);
-        return new ProductResponseDTO(
-                saved.getId(),
-                saved.getName(),
-                saved.getDescription(),
-                saved.getPrice(),
-                saved.getStock(),
-                saved.getImageUrl(),
-                new CategorySummaryDTO(
-                        saved.getCategory().getName(),
-                        saved.getCategory().getId()
-                )
-        );
+        return ProductMapper.toDTO(saved);
     }
 
     // delete product
