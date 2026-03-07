a934a43ce8c79fe1b3368502cc8baa3f0b6a5a96
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-07 15:17:32 +0000

    create a password encoder bean

---
+++ src/main/java/org/example/ecommerceapi/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package org.example.ecommerceapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author $(bilal belhaj)
+ **/
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
