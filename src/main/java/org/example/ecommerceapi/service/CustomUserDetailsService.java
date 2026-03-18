fb27642e52ca67d3378aa4617d8cf14e5f14868c
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-18 17:11:06 +0000

    create CustomUserDetailsService

---
+++ src/main/java/org/example/ecommerceapi/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package org.example.ecommerceapi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.ecommerceapi.entity.User;
+import org.example.ecommerceapi.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author $(bilal belhaj)
+ **/
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow();
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(r -> new SimpleGrantedAuthority(r.getName()))
+                        .toList()
+        );
+    }
+}
