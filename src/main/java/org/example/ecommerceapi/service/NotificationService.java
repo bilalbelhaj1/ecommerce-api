5c4bcb8feda4addc4822514d17aba3ca9302ce15
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-25 20:05:55 +0100

    implement read/delete notifications service

--- src/main/java/org/example/ecommerceapi/service/NotificationService.java
+++ src/main/java/org/example/ecommerceapi/service/NotificationService.java
@@ -5,10 +5,13 @@ import org.example.ecommerceapi.dto.notification.CreateNotificationDTO;
 import org.example.ecommerceapi.dto.notification.NotificationResponse;
 import org.example.ecommerceapi.entity.Notification;
 import org.example.ecommerceapi.entity.User;
+import org.example.ecommerceapi.exception.BadRequestException;
 import org.example.ecommerceapi.exception.ResourceNotFoundException;
 import org.example.ecommerceapi.repository.NotificationRepository;
 import org.example.ecommerceapi.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author $(bilal belhaj)
@@ -26,16 +29,38 @@ public class NotificationService {
         );
 
         Notification notification = toEntity(dto, user);
-
         return toResponse(notificationRepository.save(notification));
     }
 
     // get notifications
+    public List<NotificationResponse> getNotifications(Long userId) {
+        if (userId == null) {
+            throw new BadRequestException("Invalid request with no userId");
+        }
+        return notificationRepository.findByUserId(userId)
+                .stream()
+                .map(this::toResponse)
+                .toList();
+    }
 
     // read notification
+    public NotificationResponse read(Long id) {
+        Notification notification = notificationRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Notification not found")
+        );
+        notification.setRead(true);
+        return toResponse(notificationRepository.save(notification));
+    }
 
     // delete notification
+    public void delete(Long id) {
+        if (!notificationRepository.existsById(id)){
+            throw new ResourceNotFoundException("Notification not found");
+        }
+        notificationRepository.deleteById(id);
+    }
 
+    // mappers
     private NotificationResponse toResponse(Notification notification) {
         return new NotificationResponse(
                 notification.getType(),
