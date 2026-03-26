06296779c8cb70201d48f76d234a3ab9c4931315
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-26 20:37:50 +0100

    log unhandled exceptions

--- src/main/java/org/example/ecommerceapi/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/ecommerceapi/exception/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
-package org.example.ecommerceapi.controller;
+package org.example.ecommerceapi.exception;
 
-import org.example.ecommerceapi.exception.BadRequestException;
-import org.example.ecommerceapi.exception.ResourceNotFoundException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationException(MethodArgumentNotValidException ex) {
         var errors = ex.getBindingResult().getFieldErrors().stream()
@@ -55,6 +56,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleOtherExceptions(Exception ex) {
+        logger.error("Unhandled exception from server ", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
                 "timestamp", LocalDateTime.now(),
                 "status", 500,
