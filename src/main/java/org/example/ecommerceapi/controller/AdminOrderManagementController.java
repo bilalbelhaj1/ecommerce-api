93ceb22b667f19f7f39a02f01861995def78b74d
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-27 02:28:31 +0100

    document all the api endpoints

--- src/main/java/org/example/ecommerceapi/controller/AdminOrderManagementController.java
+++ src/main/java/org/example/ecommerceapi/controller/AdminOrderManagementController.java
@@ -1,5 +1,8 @@
 package org.example.ecommerceapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.ecommerceapi.dto.orderManagement.OrderResponseDTO;
 import org.example.ecommerceapi.dto.orderManagement.OrderSummaryDTO;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 /**
  * @author $(bilal belhaj)
  **/
+
+@Tag(name = "Orders-management-admin", description = "operations related to orders (admin)")
 @PreAuthorize("hasRole('ADMIN')")
 @RestController
 @RequestMapping("api/v1/admin/orders")
@@ -22,8 +27,12 @@ public class AdminOrderManagementController {
 
     // get orders
     @GetMapping
+    @Operation(
+            summary = "get orders",
+            description = "returns all orders with pagination and filtering"
+    )
     public ResponseEntity<Page<OrderSummaryDTO>> getOrders(
-            @RequestParam(required = false) OrderStatus status,
+            @Parameter(description = "Order Status") @RequestParam(required = false) OrderStatus status,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
@@ -32,17 +41,25 @@ public class AdminOrderManagementController {
 
     // get one order
     @GetMapping("{id}")
+    @Operation(
+            summary = "get order",
+            description = "return order details"
+    )
     public ResponseEntity<OrderResponseDTO> getOrder(
-            @PathVariable Long id
+            @Parameter(description = "Order Id") @PathVariable Long id
     ) {
         return ResponseEntity.ok().body(orderManagementService.getOrder(id));
     }
 
     // get orders by customer
     @GetMapping("/customer/{id}")
+    @Operation(
+            summary = "get customer orders",
+            description = "returns all orders of a specific customer"
+    )
     public ResponseEntity<Page<OrderSummaryDTO>> getCustomerOrders(
-            @PathVariable Long id,
-            @RequestParam(required = false) OrderStatus status,
+            @Parameter(description = "Customer Id") @PathVariable Long id,
+            @Parameter(description = "Order status") @RequestParam(required = false) OrderStatus status,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ){
@@ -51,9 +68,13 @@ public class AdminOrderManagementController {
 
     // update status
     @PutMapping("{id}")
+    @Operation(
+            summary = "update order status",
+            description = "update the order status"
+    )
     public ResponseEntity<OrderSummaryDTO> updateStatus(
-            @PathVariable Long id,
-            @RequestParam OrderStatus status
+            @Parameter(description = "Order Id") @PathVariable Long id,
+            @Parameter(description = "new Order Status") @RequestParam OrderStatus status
     ) {
         return ResponseEntity.ok().body(orderManagementService.updateStatus(id, status));
     }
