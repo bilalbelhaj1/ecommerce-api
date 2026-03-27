fca417c52df42845de3057b76667b4ee865b4802
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-03-27 02:05:26 +0100

    document rating controller

--- src/main/java/org/example/ecommerceapi/controller/RatingController.java
+++ src/main/java/org/example/ecommerceapi/controller/RatingController.java
@@ -1,5 +1,8 @@
 package org.example.ecommerceapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.ecommerceapi.dto.rating.CreateRatingDTO;
@@ -15,6 +18,7 @@ import java.util.List;
 /**
  * @author $(bilal belhaj)
  **/
+@Tag(name = "ratings", description = "Operations related to ratings")
 @RestController
 @RequestMapping("api/v1/ratings")
 @RequiredArgsConstructor
@@ -24,6 +28,10 @@ public class RatingController {
     // create
     @PreAuthorize("hasRole('CUSTOMER')")
     @PostMapping()
+    @Operation(
+            summary = "rate product",
+            description = "add a rating to a product"
+    )
     public ResponseEntity<RatingResponseDTO> rateProduct(@Valid @RequestBody CreateRatingDTO dto) {
         RatingResponseDTO res = ratingService.create(dto);
         URI uri = URI.create("http://localhost:8081/api/v1/ratings/" + res.id());
@@ -32,7 +40,11 @@ public class RatingController {
 
     // list ratings
     @GetMapping("product/{productId}")
-    public ResponseEntity<List<RatingResponseDTO>> getRatings(@PathVariable Long productId) {
+    @Operation(
+            summary = "get ratings",
+            description = "get all ratings of a product"
+    )
+    public ResponseEntity<List<RatingResponseDTO>> getRatings(@Parameter(description = "Product Id") @PathVariable Long productId) {
         return ResponseEntity.ok().body(ratingService.getAll(productId));
     }
 }
