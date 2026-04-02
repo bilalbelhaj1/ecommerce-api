aec4b9417967b93f2fe942d4f7db595d04d2ac7c
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-04-03 00:16:28 +0100

    write unit tests for NotificationController

--- src/test/java/org/example/ecommerceapi/controller/NotificationControllerTest.java
+++ src/test/java/org/example/ecommerceapi/controller/NotificationControllerTest.java
@@ -8,14 +8,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,7 +37,8 @@ class NotificationControllerTest {
     @Test
     void getNotifications() throws Exception{
         List<NotificationResponse> res = List.of(
-                new NotificationResponse("TEST", "test notification", 1L, false, LocalDateTime.now()),
+                new NotificationResponse("TEST", "test notification", 1L, false,
+                        LocalDateTime.of(LocalDate.of(2026, 4, 2), LocalTime.of(12,12,12))),
                 new NotificationResponse("TEST", "test notification 2", 2L, true, LocalDateTime.now().plusDays(1))
         ) ;
 
@@ -48,17 +50,28 @@ class NotificationControllerTest {
                 .andExpect(jsonPath("$[0].type").value("TEST"))
                 .andExpect(jsonPath("$[0].content").value(res.get(0).content()))
                 .andExpect(jsonPath("$[0].isRead").value(false))
-                .andExpect(jsonPath("$[0].createdAt").value(res.get(0).createdAt()))
                 .andExpect(jsonPath("$[1].id").value(2L))
-                .andExpect(jsonPath("$[1].isRead").value(true))
-                .andExpect(jsonPath("$[1].createdAt").value(res.get(1).createdAt()));
+                .andExpect(jsonPath("$[1].isRead").value(true));
     }
 
     @Test
-    void read() {
+    void read() throws Exception{
+        NotificationResponse res = new NotificationResponse("TEST", "test notification", 1L, true, LocalDateTime.now());
+
+        when(notificationService.read(1L)).thenReturn(res);
+
+        mockMvc.perform(put("/api/v1/notifications/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(res.id()))
+                .andExpect(jsonPath("$.content").value(res.content()))
+                .andExpect(jsonPath("$.isRead").value(true));
     }
 
+
     @Test
-    void delete() {
+    void deleteNotification() throws Exception {
+        mockMvc.perform(delete("/api/v1/notifications/{id}", 1L))
+                .andExpect(status().isNoContent());
     }
+
 }
